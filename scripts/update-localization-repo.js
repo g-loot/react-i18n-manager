a448942acda2d0bd619c0f0c2ea4dffa7dbbf27b
Omar ElGaml <omar@gloot.com>
2019-12-03 13:48:34 +0100

    polishing the library with logging and verbose logging options

--- scripts/update-localization-repo.js
+++ scripts/update-localization-repo.js
@@ -1,6 +1,13 @@
 require("colors");
 const { exec } = require("shelljs");
 const manageTranslations = require("./manageTranslations");
+let verboseLogging = false;
+const execute = command => {
+  if (verboseLogging) {
+    console.info(`executing: ${`${command}`.blue}`);
+  }
+  exec(command);
+};
 
 const syncTranslations = ({
   srcDirectory = "src",
@@ -9,30 +16,31 @@ const syncTranslations = ({
   REPO_SSH_URL = "git@github.com:g-loot/gll-play-localization.git",
   REPO_EXTRL_DIR = "gll-play-localization",
   locallyGeneratedMessagesDir = "src/i18n/locales/generated-messages",
-  options: { allowEmptyTranslations = false } = {}
+  options: { allowEmptyTranslations = false, allowVerboseLogging = false } = {}
 } = {}) => {
+  verboseLogging = allowVerboseLogging;
+
   const REPO_DIR = `../${REPO_EXTRL_DIR}`;
   const EXTRACTED_MESSAGES_DIR = `${locallyGeneratedMessagesDir}/*`;
 
-  console.info(`executing: ${`rm -rf ${REPO_DIR}`.blue}`);
-  exec(`rm -rf ${REPO_DIR}`);
-
-  console.info(`executing: ${`git clone ${REPO_SSH_URL} ${REPO_DIR}`.blue}`);
-  exec(`git clone ${REPO_SSH_URL} ${REPO_DIR}`);
-  console.info(`executing: ${`git pull`.blue}`);
-  exec(`git pull`, { cwd: REPO_DIR });
-
-  console.info(`executing: ${`rm -rf ${locallyGeneratedMessagesDir}/`.blue}`);
-  exec(`rm -rf ${locallyGeneratedMessagesDir}/`);
+  console.info("Cloning localization files repository".magenta);
+  execute(`rm -rf ${REPO_DIR}`);
+  execute(`git clone ${REPO_SSH_URL} ${REPO_DIR}`);
+  execute(`git pull`, { cwd: REPO_DIR });
+  execute(`rm -rf ${locallyGeneratedMessagesDir}/`);
+  execute(`mkdir ${locallyGeneratedMessagesDir}`);
+  execute(`cp -r ${REPO_DIR}/ ${locallyGeneratedMessagesDir}/`);
   console.info(
-    `executing: ${`cp -r ${REPO_DIR}/ ${locallyGeneratedMessagesDir}/`.blue}`
+    `Extracting Messages from Source code path ${
+      `${srcDirectory} into ${EXTRACTED_MESSAGES_DIR}`.blue
+    }`.magenta
   );
-  exec(`mkdir ${locallyGeneratedMessagesDir}`);
-  exec(`cp -r ${REPO_DIR}/ ${locallyGeneratedMessagesDir}/`);
-  console.info(`executing: ${"npm run update-translations".blue}`);
-  // exec('npm run update-translations');
-  exec(
+  execute(
     `NODE_ENV=production babel ./${srcDirectory} --ignore '${EXTRACTED_MESSAGES_DIR}'  --out-file /dev/null `
+  );
+  console.info(
+    "Generating language files and export file from extracted messages and merging them into localization repository"
+      .magenta
   );
   manageTranslations({
     supportedLocales,
@@ -40,26 +48,21 @@ const syncTranslations = ({
     locallyGeneratedMessagesDir,
     allowEmptyTranslations
   });
-  console.info(
-    `executing: ${`cp -r ${locallyGeneratedMessagesDir}/ ${REPO_DIR}/`.blue}`
+  console.log(
+    `Copying results back to original repository and pushing changes,.  `
+      .magenta
   );
-  exec(`cp -r ${locallyGeneratedMessagesDir}/ ${REPO_DIR}/`);
-  console.info(`executing: ${"git add .".blue}`);
-  exec("git add .", { cwd: REPO_DIR });
-  // console.info(
-  //   `executing: ${`git commit -a -m "Automated translation ids update"`.blue}`
-  // );
-  // exec(`git commit -a -m "Automated translation ids update"`, {
-  //   cwd: REPO_DIR
-  // });
-  // console.info(`executing: ${`git push`.blue}`);
-  // exec(`git push`, { cwd: REPO_DIR });
-  // exec(`npm run publish`, { cwd: REPO_DIR });
-  // exec(`npm up gll-play-localization`);
-  // console.info(`executing: ${`rm -rf ${REPO_DIR}`.blue}`);
-  // // exec(`rm -rf ${REPO_DIR}`);
-  // exec(`rm -rf ${locallyGeneratedMessagesDir}`);
-  // exec("rm -rf src/i18n/locales/extracted-messages");
+  execute(`cp -r ${locallyGeneratedMessagesDir}/ ${REPO_DIR}/`);
+  execute("git add .", { cwd: REPO_DIR });
+  execute(`git commit -a -m "Automated translation ids update"`, {
+    cwd: REPO_DIR
+  });
+  execute(`git push`, { cwd: REPO_DIR });
+  execute(`npm run publish`, { cwd: REPO_DIR });
+  execute(`npm up gll-play-localization`);
+  execute(`rm -rf ${REPO_DIR}`);
+  execute(`rm -rf ${locallyGeneratedMessagesDir}`);
+  execute("rm -rf src/i18n/locales/extracted-messages");
 };
 
 module.exports = syncTranslations;
